adf9da715b22e2ac217a60abc779af592503a092
PravallikaReddyVemireddy <101160080+PravallikaReddyVemireddy@users.noreply.github.com>
2022-03-22 15:29:13 +0530

    Add files via upload

---
+++ pp.ts
@@ -0,0 +1,6 @@
+function sub(a,b)
+{
+	return a-b;
+}
+var s=sub(5,3);
+console.log(s);
---
+++ v.ts
@@ -0,0 +1,9 @@
+function sub(a,b,c?)
+{
+	return a-b;
+}
+var s=sub("sir","hi",0);
+console.log(s);
+var arr:number[];
+arr=[7,8];
+console.log(arr[1]);
