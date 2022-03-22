59b73e8f71ca3a3836721358f54e10476a41056b
PravallikaReddyVemireddy <101160080+PravallikaReddyVemireddy@users.noreply.github.com>
2022-03-22 15:34:53 +0530

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,10 @@
+class New
+{
+	first:string;
+	sec:number;
+}
+var o=new New();
+o.first="hi";
+o.sec=5;
+console.log(o.first);
+console.log(o.sec);
---
+++ func.ts
@@ -0,0 +1,6 @@
+function f(a:number,b=9)
+{
+	return a+b;
+}
+var s=f(4,8);
+console.log(s);
